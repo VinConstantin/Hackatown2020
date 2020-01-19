0e8e8731855b35551d158b846a9316991470cad8
Vincent Constantin <vincent.constantin.1@ens.etsmtl.ca>
2020-01-18 19:38:58 -0500

    client advencement

--- client/app/src/assets/js/socketService.js
+++ client/app/src/assets/js/socketService.js
@@ -1,29 +1,28 @@
 import openSocket from 'socket.io-client';
 
 var socket;
-var socket;
+var dashboard;
+
 export const Run = ()=>{
-    const URL = "http://localhost"
+    const URL = "http://localhost:4000"
     socket = openSocket(URL);
 
-    socket.on('tollRoad', (price) => {
-
-    })
-    socket.on('offToll', () => {
-        
-    })
     //update position every 30s
     setInterval(getPosition, 3000);
-
 }
 
 function getPosition(){
+    
     navigator.geolocation.getCurrentPosition(emitPosition);
 }
 function emitPosition(position){
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
     console.log(long +" , "+ lat)
-    socket.emit('checkCoords',long,lat);
+    socket.emit('checkCoords', long, lat, (price) => {
+        var event = new CustomEvent('update', { lat, long });
+        document.getElementById("content").dispatchEvent(event);
+        console.log(price)
+    })
 }
 
--- client/app/src/views/Dashboard.jsx
+++ client/app/src/views/Dashboard.jsx
@@ -1,21 +1,5 @@
-/*!
 
-=========================================================
-* Paper Dashboard React - v1.1.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 // react plugin used to create charts
 import { Line, Pie } from "react-chartjs-2";
@@ -29,6 +13,7 @@ import {
   Row,
   Col
 } from "reactstrap";
+import Map from "./Map"
 // core components
 import {
   dashboard24HoursPerformanceChart,
@@ -36,11 +21,25 @@ import {
   dashboardNASDAQChart
 } from "variables/charts.jsx";
 
+
 class Dashboard extends React.Component {
+  componentDidMount() {
+    document.getElementById("content").addEventListener("update", this.handleUpdate);
+  }
+
+  componentWillUnmount() {
+    document.getElementById("content").removeEventListener("update", this.handleUpdate);
+  }
+
+  handleUpdate = (event) => {
+    window.mapComponent.update();
+    console.log("update dashboard", event);
+  }
+
   render() {
     return (
       <>
-        <div className="content">
+        <div className="content" id="content">
           <Row>
             <Col lg="3" md="6" sm="6">
               <Card className="card-stats">
@@ -48,23 +47,19 @@ class Dashboard extends React.Component {
                   <Row>
                     <Col md="4" xs="5">
                       <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-globe text-warning" />
+                        <i className="nc-icon nc-watch-time text-warning" />
                       </div>
                     </Col>
                     <Col md="8" xs="7">
                       <div className="numbers">
-                        <p className="card-category">Capacity</p>
-                        <CardTitle tag="p">150GB</CardTitle>
+                        <p className="card-category">Current time</p>
+                        <CardTitle tag="cBilling">1h50</CardTitle>
                         <p />
                       </div>
                     </Col>
                   </Row>
                 </CardBody>
                 <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fas fa-sync-alt" /> Update Now
-                  </div>
                 </CardFooter>
               </Card>
             </Col>
@@ -79,150 +74,67 @@ class Dashboard extends React.Component {
                     </Col>
                     <Col md="8" xs="7">
                       <div className="numbers">
-                        <p className="card-category">Revenue</p>
-                        <CardTitle tag="p">$ 1,345</CardTitle>
+                        <p className="card-category">current billing</p>
+                        <CardTitle tag="p">20,50 $</CardTitle>
                         <p />
                       </div>
                     </Col>
                   </Row>
                 </CardBody>
                 <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-calendar" /> Last day
-                  </div>
                 </CardFooter>
               </Card>
             </Col>
+
             <Col lg="3" md="6" sm="6">
               <Card className="card-stats">
                 <CardBody>
                   <Row>
                     <Col md="4" xs="5">
                       <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-vector text-danger" />
+                        <i className="nc-icon nc-money-coins text-warning" />
                       </div>
                     </Col>
                     <Col md="8" xs="7">
                       <div className="numbers">
-                        <p className="card-category">Errors</p>
-                        <CardTitle tag="p">23</CardTitle>
+                        <p className="card-category">Total billing</p>
+                        <CardTitle tag="cBilling">120,00$</CardTitle>
                         <p />
                       </div>
                     </Col>
                   </Row>
                 </CardBody>
                 <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-clock" /> In the last hour
-                  </div>
                 </CardFooter>
               </Card>
             </Col>
+
             <Col lg="3" md="6" sm="6">
               <Card className="card-stats">
                 <CardBody>
                   <Row>
                     <Col md="4" xs="5">
                       <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-favourite-28 text-primary" />
+                        <i className="nc-icon nc-money-coins text-warning" />
                       </div>
                     </Col>
                     <Col md="8" xs="7">
                       <div className="numbers">
-                        <p className="card-category">Followers</p>
-                        <CardTitle tag="p">+45K</CardTitle>
+                        <p className="card-category">Credit left</p>
+                        <CardTitle tag="cBilling">45,50$</CardTitle>
                         <p />
                       </div>
                     </Col>
                   </Row>
                 </CardBody>
                 <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fas fa-sync-alt" /> Update now
-                  </div>
                 </CardFooter>
               </Card>
             </Col>
           </Row>
           <Row>
             <Col md="12">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Users Behavior</CardTitle>
-                  <p className="card-category">24 Hours performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboard24HoursPerformanceChart.data}
-                    options={dashboard24HoursPerformanceChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-history" /> Updated 3 minutes ago
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="4">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Email Statistics</CardTitle>
-                  <p className="card-category">Last Campaign Performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Pie
-                    data={dashboardEmailStatisticsChart.data}
-                    options={dashboardEmailStatisticsChart.options}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="legend">
-                    <i className="fa fa-circle text-primary" /> Opened{" "}
-                    <i className="fa fa-circle text-warning" /> Read{" "}
-                    <i className="fa fa-circle text-danger" /> Deleted{" "}
-                    <i className="fa fa-circle text-gray" /> Unopened
-                  </div>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-calendar" /> Number of emails sent
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-            <Col md="8">
-              <Card className="card-chart">
-                <CardHeader>
-                  <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
-                  <p className="card-category">Line Chart with Points</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboardNASDAQChart.data}
-                    options={dashboardNASDAQChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="chart-legend">
-                    <i className="fa fa-circle text-info" /> Tesla Model S{" "}
-                    <i className="fa fa-circle text-warning" /> BMW 5 Series
-                  </div>
-                  <hr />
-                  <div className="card-stats">
-                    <i className="fa fa-check" /> Data information certified
-                  </div>
-                </CardFooter>
-              </Card>
+              <Map/>
             </Col>
           </Row>
         </div>
